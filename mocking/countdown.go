5100cdb06d22454067c184db3796f9212a10da38
Maycon Rayone <maycon.rayone@gmail.com>
2022-12-04 17:04:54 -0300

    refact(countdown): remove defaultSleeper

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -15,12 +15,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
-
-func (ds *DefaultSleeper) Sleep() {
-	time.Sleep(time.Second * 1)
-}
-
 func Countdown(out io.Writer, s Sleeper) {
 	for i := countStarter; i > 0; i-- {
 		fmt.Fprintln(out, i)
