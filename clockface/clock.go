f497421e9773225c6938d44132a83dcdb8d4a73c
Maycon Rayone <maycon.rayone@gmail.com>
2022-12-27 21:26:57 -0300

    feat: clock circles

--- clockface/clock.go
+++ clockface/clock.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // A Point represents a two dimensional Cartesian coordinate
 type Point struct {
@@ -12,4 +15,16 @@ type Point struct {
 // represented as a Point.
 func SecondHand(t time.Time) Point {
 	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / (30 / (float64(t.Second()))))
+}
+
+func secondHandPoint(t time.Time) Point {
+	angle := secondsInRadians(t)
+	x := math.Sin(angle)
+	y := math.Cos(angle)
+
+	return Point{x, y}
 }
--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-	"net/http"
-	"os"
-	"time"
-
-	"github.com/mrayone/learn-go/di"
-	"github.com/mrayone/learn-go/mocking"
+	"fmt"
+	"math"
 )
 
-func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
-	di.Greet(w, "world")
-}
+// import (
+// 	"net/http"
+// 	"os"
+// 	"time"
+
+// 	"github.com/mrayone/learn-go/di"
+// 	"github.com/mrayone/learn-go/mocking"
+// )
+
+// func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+// 	di.Greet(w, "world")
+// }
+
+// func main() {
+// 	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
+// 	sleeper := &mocking.ConfigurableSleeper{Duration: 1 * time.Second, SleepFunc: time.Sleep}
+// 	mocking.Countdown(os.Stdout, sleeper)
+// }
 
 func main() {
-	// log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
-	sleeper := &mocking.ConfigurableSleeper{Duration: 1 * time.Second, SleepFunc: time.Sleep}
-	mocking.Countdown(os.Stdout, sleeper)
+	fmt.Println(secondsinradians())
+}
+
+func zero() float64 {
+	return 0.0
+}
+
+func secondsinradians() float64 {
+	return (math.Pi / (30 / (float64(zero()))))
 }
