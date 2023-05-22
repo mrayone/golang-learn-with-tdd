1e981b804cefb427ebca7a0ced35d5e3d42cbc50
Maycon <maycon.rayone@gmail.com>
2023-05-22 11:11:21 -0300

    feat(sorting): add searching test

---
+++ computerscience/sortingsearch.go
@@ -0,0 +1,62 @@
+package computerscience
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"strconv"
+)
+
+func binarySearchRecurse(lo, hi int, scanner *bufio.Scanner) (int, error) {
+	if hi-lo == 1 {
+		return lo, nil
+	}
+
+	mid := lo + (hi-lo)/2
+	fmt.Printf("Greater than or equal to %d? ", mid)
+	err := scanner.Err()
+	if err == nil {
+		scanner.Scan()
+		text := scanner.Text()
+		isTrue, err := strconv.ParseBool(text)
+		if err != nil {
+			return 0, errors.New("you must given a valid boolean assertion, true or false")
+		}
+
+		if isTrue {
+			return binarySearchRecurse(lo, mid, scanner)
+		} else {
+			return binarySearchRecurse(mid, hi, scanner)
+		}
+	}
+
+	return 0, err
+}
+
+// PlayTwentyQuestions for given integer input
+func PlayTwentyQuestions(rd io.Reader) (string, error) {
+	scanner := bufio.NewScanner(rd)
+	err := scanner.Err()
+	if err != nil {
+		return "", errors.New("you must give a number")
+	}
+	scanner.Scan()
+
+	input := scanner.Text()
+	k, err := strconv.Atoi(input)
+	if err != nil {
+		return "", errors.New("the value of input must be a valid integer")
+	}
+
+	n := int(math.Pow(2, float64(k)))
+	fmt.Printf("Think of a number between 0 and %d\n", (n - 1))
+
+	guess, err := binarySearchRecurse(0, n, scanner)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("Your number is %d\n", guess), nil
+}
---
+++ computerscience/sortingsearch_test.go
@@ -0,0 +1,71 @@
+package computerscience_test
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/mrayone/learn-go/computerscience"
+)
+
+func TestPlayTweentyQuestions(t *testing.T) {
+	var mockReader bytes.Buffer
+
+	testCases := []struct {
+		desc          string
+		inputs        []string
+		expectedValue string
+		expectedErr   error
+	}{
+		{
+			desc: "guess the 77 number",
+			inputs: []string{
+				fmt.Sprintf("%d\n", 7), // questions quantity
+				"false\n",
+				"true\n",
+				"true\n",
+				"false\n",
+				"false\n",
+				"true\n",
+				"false\n",
+			},
+			expectedValue: fmt.Sprintf("Your number is %d\n", 77),
+		},
+		{
+			desc: "guess the 44 number",
+			inputs: []string{
+				fmt.Sprintf("%d\n", 6), // questions quantity
+				"false\n",
+				"true\n",
+				"false\n",
+				"false\n",
+				"true\n",
+				"true\n",
+			},
+			expectedValue: fmt.Sprintf("Your number is %d\n", 44),
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			for _, val := range tc.inputs {
+				mockReader.Write([]byte(val))
+			}
+
+			guess, err := computerscience.PlayTwentyQuestions(&mockReader)
+
+			assertError(t, err, tc.expectedErr)
+
+			if guess != tc.expectedValue {
+				t.Errorf("unexpected value %q given %q want", guess, tc.expectedValue)
+			}
+		})
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q given", got, want)
+	}
+}
